640e71ba2c5f578543b2524ee19761bf00b8abd9
Alister Lewis-Bowen <alister@different.com>
2013-10-15 15:38:50 -0400

    initial commit

---
+++ namegen.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+BASE=$(dirname $0)
+WORDS=''
+NUM_WORDS=0
+PHRASE=''
+WORDS_IN_PHRASE=3
+
+function importWords {
+    local i=0
+    while read word ; do
+        WORDS[$i]="$word"
+        i=$(($i+1))
+    done < "$BASE/words.txt"
+    NUM_WORDS=${#WORDS[@]}
+}
+
+function getWord {
+    local word="$1"
+    while [[ $(echo "$1" | grep -c "$word") -gt 0 ]]; do
+        word=${WORDS[$(( ( RANDOM % $NUM_WORDS ) ))]}
+    done
+    echo -n "$word "
+}
+
+# @see http://phodd.net/gnu-bc/
+function permutation {
+    echo 'define int_permutation(n,r)
+    {
+        auto i,p,os;
+        os=scale;scale=0;n/=1;r/=1
+        if(n<0||r<0||r>n)return(0)
+        p=1;for(i=n;i>n-r;i--)p*=i
+        scale=os;return(p)
+    }
+    int_permutation('$1','$2')' | bc
+}
+
+importWords && (
+    echo "Imported $NUM_WORDS words"
+    COMBINATIONS=$(permutation $NUM_WORDS $WORDS_IN_PHRASE)
+    echo "That provides $COMBINATIONS combinations of a $WORDS_IN_PHRASE word phrase"
+    echo -e "Like this...\n"
+    for combinations in $(seq $COMBINATIONS); do
+        for words in $(seq $WORDS_IN_PHRASE); do
+            PHRASE=$PHRASE$(getWord "$PHRASE")
+        done
+        echo $PHRASE
+        PHRASE=''
+    done
+    echo )
