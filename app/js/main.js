d1e4a28514cf06975abde07c40ce5fa0655577c0
Alister Lewis-Bowen <alister@different.com>
2013-10-17 10:24:54 -0400

    some minor refactoring

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
+/**
+ * ★ A simple toy to brainstorm a name or phrase.
+ * @author Alister Lewis-Bowen <alister@different.com>
+ */
 
 var words = [],
     kickstart = ['Beacon','Insight','Monitor','Mobile','Case','View','Scope',
@@ -13,6 +17,9 @@ var words = [],
     phraseTimer = null,
     entryShown = false,
     wordsShown = false,
+
+    // Return a word from the words array that hasn't already been used
+    // in the phrase
     getWord = function(phrase, words) {
         if (words.length === 0) return '';
         var word = phrase;
@@ -21,7 +28,9 @@ var words = [],
         }
         return word +' ';
     },
-    getPhrase = function(words) {
+
+    // Return a phrase of a specified number of words
+    getPhrase = function(words, wordsPerPhrase) {
         if (words.length < wordsPerPhrase) {
             setStatus('Add a word', false);
             return '';
@@ -32,6 +41,29 @@ var words = [],
         }
         return phrase;
     },
+
+    // Display a phrase on-screen
+    setPhrase = function(phrase) {
+        if (typeof(phrase) === 'undefined') phrase = getPhrase(words, wordsPerPhrase);
+        console.log('set phrase to '+ phrase);
+        $('#phrase').fadeOut(function() {
+            $(this).text(phrase).fadeIn();
+       });
+    },
+
+    // Start the phrase display timer
+    startPhrase = function() {
+        console.log('Start phrase timer');
+        phraseTimer = setInterval(function() { setPhrase(); }, phraseRefresh);
+    },
+
+    // Stop the phrase display timer
+    stopPhrase = function() {
+        console.log('Stop phrase timer');
+        clearInterval(phraseTimer);
+    },
+
+    // Display a status message on-screen
     setStatus = function(message, fadeOut) {
         if (typeof(fadeOut) === 'undefined') fadeOut = true;
         $('#status').fadeIn(function() {
@@ -39,16 +71,13 @@ var words = [],
             if (fadeOut) setInterval(function() { $('#status').fadeOut(); }, 4000);
         });
     },
-    setPhrase = function(phrase) {
-        if (typeof(phrase) === 'undefined') phrase = getPhrase(words);
-        console.log('set phrase to '+ phrase);
-        $('#phrase').fadeOut(function() {
-            $(this).text(phrase).fadeIn();
-       });
-    },
+
+    // Add a word to the word list on-screen
     appendWord = function(word) {
         $('#words').append('<p><i class="icon-remove-sign pull-left icon-muted"></i>'+ word +'</p>');
     },
+
+    // Diplay all words as a list on-screen
     showWords = function(words) {
         for (var i=0;i<words.length;i++) { appendWord(words[i]); }
         $('#words').column({
@@ -58,6 +87,8 @@ var words = [],
             rule_color: '#2A2A2A',
         });
     },
+
+    // Remove word from word list and from word array
     removeWord = function(oWord) {
         var i= $.inArray(oWord.text(), words);
         if (i >= 0) {
@@ -66,8 +97,12 @@ var words = [],
             console.log('Removed '+ oWord.text());
         }
     },
-    parseKey = function(e) {
-        console.log(e.which);
+
+    // Handle any key stroke events on-screen
+    keyupHandler = function(e) {
+        //console.log(e.which);
+
+        // Display the entry field on-screen if it's not already
         if (!entryShown) {
             $('#entry').show();
             // TODO: Attempt to show trigger char?
@@ -75,63 +110,63 @@ var words = [],
             $('#entry input').focus();
             entryShown = true;
         }
+        // Hide the word list if ESC pressed
         if (wordsShown && e.which === 27) {
             $('#words').hide();
             $('#phrase').show();
             startPhrase();
             wordsShown = false;
         }
+        // Parse the entry field content if Enter or ESC pressed, then close it
         if (entryShown && (e.which === 13 || e.which === 27)) {
-            var entry = $('#entry input').val();
-            switch(entry) {
-                case ':clear':
-                    words = [];
-                    $('#words').empty();
-                    console.log('Cleared all words');
-                    break;
-                case ':kickstart':
-                    words = kickstart;
-                    console.log('Loaded test words');
-                    break;
-                case ':more':
-                    wordsPerPhrase++;
-                    setStatus('Now showing a '+ wordsPerPhrase +' word phrase');
-                    break;
-                case ':less':
-                    wordsPerPhrase--;
-                    setStatus('Now showing a '+ wordsPerPhrase +' word phrase');
-                    break;
-                case ':words':
-                    $('#words').show();
-                    $('#words').empty();
-                    showWords(words);
-                    $('#phrase').hide();
-                    stopPhrase();
-                    wordsShown = true;
-                    break;
-                default:
-                    entry = $.trim(entry);
-                    // TODO: Check for duplicates
-                    if (entry != '') {
-                        words.push(entry);
-                        setStatus('Added '+ entry);
-                        // TODO: Update localStorage
-                    }
-            }
+            parseEntry($('#entry input').val());
             $('#entry').hide();
             $('#entry input').val('');
             entryShown = false;
         }
         e.preventDefault(); // one way to stop space from scrolling the view port
     },
-    startPhrase = function() {
-        console.log('Start phrase timer');
-        phraseTimer = setInterval(function() { setPhrase(); }, phraseRefresh);
+
+    // Parse the entry field content
+    parseEntry = function (entry) {
+        switch(entry) {
+            case ':clear':      // clear the words array
+                words = [];
+                $('#words').empty();
+                console.log('Cleared all words');
+                break;
+            case ':kickstart':  // populate word aray with test data
+                words = kickstart;
+                console.log('Loaded test words');
+                break;
+            case ':more':       // increase word length of the phrase
+                wordsPerPhrase++;
+                setStatus('Now showing a '+ wordsPerPhrase +' word phrase');
+                break;
+            case ':less':       // decrease word length of the phrase
+                wordsPerPhrase--;
+                setStatus('Now showing a '+ wordsPerPhrase +' word phrase');
+                break;
+            case ':words':      // show word list
+                stopPhrase();
+                $('#phrase').hide();
+                $('#words').show();
+                $('#words').empty();
+                showWords(words);
+                wordsShown = true;
+                break;
+            default:            // add content as a word to the word array
+                entry = $.trim(entry);
+                // TODO: Check for duplicates
+                if (entry != '') {
+                    words.push(entry);
+                    setStatus('Added '+ entry);
+                    // TODO: Update localStorage
+                }
+        }
     },
-    stopPhrase = function() {
-        console.log('Stop phrase timer');
-        clearInterval(phraseTimer);
-    },
+
+    // Return number of permutations of r selected items out of n items
     permutation = function(n, r) {
         if (n<0 || r<0 || r>n) return 0;
         var p = 1; for ( var i=n; i>(n-r); i--) { p=p*i; }
@@ -145,7 +180,7 @@ $(function() {
     var perms = permutation(words.length, wordsPerPhrase);
     console.log('This provides '+ perms +' permutations of a '+ wordsPerPhrase +' word phrase');
 
-    $(document).keyup(parseKey);
+    $(document).keyup(keyupHandler);
     $('#words').delegate('p', 'click', function() { removeWord($(this)); });
 
     setPhrase();
